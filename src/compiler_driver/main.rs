13fe10cb69a279e7ee65c3b38811b31e36d094aa
xarkenz <seanedwardsclarke@gmail.com>
2024-01-05 17:02:01 -0600

    improved output of compiler_driver

--- src/compiler_driver/main.rs
+++ src/compiler_driver/main.rs
@@ -1,7 +1,14 @@
+use std::time::Instant;
+
 fn main() {
+    let start_time = Instant::now();
+
     let args = compiler::cli::parse_command_line_args();
     match compiler::cli::invoke(args) {
-        Err(error) => eprintln!("compiler_driver: {error}"),
-        Ok(_) => println!("compiler_driver: success"),
+        Err(error) => println!("\x1b[31mError: {error}\x1b[0m"),
+        Ok(_) => println!("\x1b[32mFinished\x1b[0m"),
     }
+
+    let time_taken_ms = start_time.elapsed().as_millis();
+    println!("\x1b[2mTime: {time_taken_ms} ms\x1b[22m");
 }
